eba549f4615546ff96b798c11f76549bdc46fedb
Molly F <mollymf517@gmail.com>
2022-01-18 16:37:57 -0500

    created starting template including curate class to run the program

---
+++ curate.java
@@ -0,0 +1,6 @@
+public class curate {
+    public static void main(String args[]){
+        System.out.println("Performing miRNA Curation.");
+        mapping.precursor_mapping("hello");
+    }
+}
---
+++ mapping.java
@@ -0,0 +1,7 @@
+public class mapping {
+    public static boolean precursor_mapping(String seq){
+        String out = "Input seq: " + seq;
+        System.out.println(out);
+        return true;
+    }
+}
