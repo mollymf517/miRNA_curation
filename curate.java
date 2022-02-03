a7323905dccaaa17e974708cb4130c2a3f32c4ef
Molly F <mollymf517@gmail.com>
2022-02-03 13:13:54 -0500

    fixed line calling get_precursor method to reflect name change

--- curate.java
+++ curate.java
@@ -4,7 +4,7 @@ public class curate {
     public static void main(String args[]){
         System.out.println("Performing miRNA Curation.");
         try {
-            mapping.precursor_mapping("miRNA_master.tsv");
+            mapping.get_precursors("miRNA_master.tsv");
         } catch (FileNotFoundException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
