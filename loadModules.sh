66c79779761403490ca7d6182f6e1d2b248afb15
Molly F <mollymf517@gmail.com>
2022-03-05 17:23:49 -0500

    loadModules script added to load all modules needed in one step

---
+++ loadModules.sh
@@ -0,0 +1,11 @@
+#needed for BLAST
+module load StdEnv/2020
+module load nixpkgs/16.09
+module load gcc/9.3.0
+module load gcc/7.3.0
+module load blast+/2.10.1
+#needed for BlAST
+
+#put modules for alignment here
+
+#^
